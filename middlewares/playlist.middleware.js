fdcc81ddac5daa63952d132337511382d191a258
shubham-ghuge <shubhamghuge34@gmail.com>
2021-07-03 13:52:01 +0530

    refactored end points and completed all routes

--- middlewares/playlist.middleware.js
+++ middlewares/playlist.middleware.js
@@ -1,19 +1,33 @@
 const Playlist = require("../models/playlist.model");
 
-function isPlaylistExist(req, res, next) {
+async function isPlaylistExist(req, res, next) {
     const { userId } = req.user;
     const { name } = req.body;
-    try {
-        const checkPlaylist = Playlist.findOne({ userId, name }).lean();
-        if (!checkPlaylist) {
-            next();
-        } else {
-            res.json({ success: false, message: "playlist already exists" });
+    if (name) {
+        try {
+            const checkPlaylist = await Playlist.findOne({ userId, name }).lean();
+            if (!checkPlaylist) {
+                next();
+            } else {
+                res.json({ success: false, message: "playlist already exists" });
+            }
+        } catch (error) {
+            console.log("error in playlist middleware", error);
+            res.json({ success: true, message: "error while creating playlist" });
         }
-    } catch (error) {
-        console.log("error in playlist middleware", error);
-        res.json({ success: true, message: "error while creating playlist" });
+    } else {
+        res.json({ success: false, message: "insuffiecient data" })
     }
 }
 
-module.exports = { isPlaylistExist }
+async function checkPlaylistById(req, res, next, id) {
+    try {
+        await Playlist.findById(id);
+        next();
+    } catch (error) {
+        console.log("error in playlist middleware", error)
+        res.json({ success: false, message: "playlist doesnt exist" })
+    }
+}
+
+module.exports = { isPlaylistExist, checkPlaylistById }
--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -2,19 +2,20 @@ const express = require('express');
 const Playlist = require('../models/playlist.model');
 const router = express.Router();
 const { authHandler } = require('../middlewares/auth.middleware')
-const { isPlaylistExist } = require('../middlewares/playlist.middleware')
+const { isPlaylistExist, checkPlaylistById } = require('../middlewares/playlist.middleware')
 
 router.route('/')
     .get(authHandler, async (req, res) => {
         const { userId } = req.user;
         try {
-            const response = await Playlist.find({ userId }).populate("videos").exec();
+            const userData = await Playlist.find({ userId });
+            const response = userData.map(({ name, _id, videos }) => ({ name, _id, length: videos.length }));
             res.json({ success: true, message: "playlist data", response });
         } catch (error) {
             console.log(error);
             res.json({ success: false, message: "error occured while retrieving playlist data" });
         }
-    })
+    })/* will return all the playlist data of user */
     .post(authHandler, isPlaylistExist, async (req, res) => {
         const { userId } = req.user;
         const { name, videoId } = req.body;
@@ -25,28 +26,53 @@ router.route('/')
             console.log("error while creating playlist", error);
             res.json({ success: false, message: "error while creating playlist", error });
         }
-    })
-    .patch(authHandler, async (req, res) => {
+    })/* will create playlist for user */
+
+
+router.param("playlistId", checkPlaylistById)
+router.route('/:playlistId')
+    .get(authHandler, async (req, res) => {
         const { userId } = req.user;
-        const { name, videoId } = req.body;
+        const { playlistId } = req.params;
         try {
-            await Playlist.findOneAndUpdate({ userId, name }, { $push: { videos: videoId } });
-            res.json({ success: true, message: `video added in playlist ${name}` })
+            const response = await Playlist.findOne({ userId, _id: playlistId }).populate("videos").exec();
+            res.json({ success: true, response });
+        } catch (error) {
+            console.log("error in retrieving playlist data", error)
+            res.json({ success: false, message: "something went wrong" });
+        }
+    })/* will return particular playlist data */
+    .delete(authHandler, async (req, res) => {
+        const { playlistId } = req.params;
+        try {
+            await Playlist.findByIdAndDelete(playlistId);
+            res.json({ success: true, message: "playlist deleted" });
+        } catch (error) {
+            console.log(error);
+            res.json({ success: false, message: "something went wrong" });
+        }
+    })/* will delete particular playlist */
+
+router.route('/:playlistId/:videoId')
+    .patch(authHandler, async (req, res) => {
+        const { playlistId, videoId } = req.params;
+        try {
+            await Playlist.findByIdAndUpdate(playlistId, { $push: { videos: videoId } });
+            res.json({ success: true, message: `video added in playlist` })
         } catch (error) {
             console.log("error while updating playlist", error);
             res.json({ success: false, message: "error while updating playlist" })
         }
-    })
+    })/* will add videos in playlist */
     .delete(authHandler, async (req, res) => {
-        const { userId } = req.user;
-        const { name, videoId } = req.body;
+        const { playlistId, videoId } = req.params;
         try {
-            await Playlist.findOneAndUpdate({ userId, name }, { $pull: { videos: videoId } });
-            res.json({ success: true, message: `video removed from ${name} playlist` });
+            await Playlist.findByIdAndUpdate(playlistId, { $pull: { videos: videoId } });
+            res.json({ success: true, message: `video removed from playlist` });
         } catch (error) {
             console.log("error in deleting playlist", error);
             res.json({ success: false, message: "error in deleting" })
         }
-    })
+    })/* will delete video from playlist */
 
 module.exports = router;
